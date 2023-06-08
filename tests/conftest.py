84a98264f254801ee999d65a0b3e14a67cb76d7d
Natom78 <99360304+Nathom78@users.noreply.github.com>
2023-06-08 10:59:32 +0200

    test with server run with process command, but no mocker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 import server
+import socket
+import subprocess
+import os
 
 
 @pytest.fixture
@@ -10,16 +13,35 @@ def client():
         yield client
 
 
-
-
 @pytest.fixture
 def clubs_fixture():
     """This is the fixture that sets a fake list of clubs for the tests."""
     clubs = {
         "clubs": [
-            {"name": "club_1", "email": "club_1@club_a.com", "points": 20},
-            {"name": "club_2", "email": "club_2@club_b.com", "points": 20},
-            {"name": "club_3", "email": "club_3@club_c.com", "points": 20}
+            {"name": "club_1", "email": "club_1@cluba.com", "points": 20},
+            {"name": "club_2", "email": "club_2@clubb.com", "points": 20},
+            {"name": "club_3", "email": "club_3@clubc.com", "points": 20}
         ]
     }
     return clubs
+
+
+# @pytest.fixture(scope="session")
+# def flask_port():
+#     # Ask OS for a free port.
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.bind(("", 0))
+#         addr = s.getsockname()
+#         port = addr[1]
+#         return 4099
+
+
+@pytest.fixture(scope="session", autouse=True)
+def live_server():
+    # env = os.environ.copy()
+    # env["FLASK_APP"] = "server"
+    live_server = subprocess.Popen(['flask', '--app', 'server', 'run', '--port', str(4099)])
+    try:
+        yield live_server
+    finally:
+        live_server.terminate()
